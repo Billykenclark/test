c6da4ad10eaf8131b6028a4c4c2acd34a411edaa
billykenclark <root@DNSServer>
2017-02-04 13:31:14 +0800

    test

---
+++ src/gitTest.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : gitTest.cpp
+// Author      : h
+// Version     :
+// Copyright   : h
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "h" << endl; // prints h
+	return 0;
+}
